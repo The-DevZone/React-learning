494e6993f7ab42519a6a39865c0b057d2813c5f1
unknown <rohit995379@gmail.com>
2025-06-30 23:56:17 +0530

    Added student list with filters and count logic

--- loop/src/App.jsx
+++ loop/src/App.jsx
@@ -26,11 +26,7 @@ function App() {
         { id: 8, name: "Kiran", marks: 91 },
         { id: 9, name: "Mohit", marks: 34 },
         { id: 10, name: "Neha", marks: 0 },
-        { id: 11, name: "Deepak", marks: 7 },
-        { id: 12, name: "Preeti", marks: 0 },
-        { id: 13, name: "Suresh", marks: 4 },
-        { id: 99, name: "Kshitij", marks: 8 },
-        { id: 100, name: "Mehak", marks: 86 }
+        
     ];
 
     let count = 0;
@@ -59,6 +55,7 @@ function App() {
                 ))
                 }
             </ul> */}
+            <h2>This is a practice student Name length</h2>
             <ul>
                 {students
                     .filter(student => student.name.length <= 3)
@@ -66,6 +63,13 @@ function App() {
                         <li key={student.id}>This is student = {student.name}</li>
                     ))}
             </ul>
+            <h2>This is a practice for the odd number</h2>
+            {students
+            .filter(student => student.id % 2 !== 0).map((student) =>(
+                <li> This Odd number  data render {student.name} </li>
+            ))
+
+            }
 
             <button> This is count:<b>{count}</b></button>
         </>
